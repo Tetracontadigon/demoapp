6c866defc26e1b1d6ef835cd88939df09a2f4dfd
tetracontadi <lucan.kesyer@gmail.com>
2022-03-24 15:01:13 -0400

    first commit again again

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Amplify from 'aws-amplify';
+import "@aws-amplify/ui-react/styles.css";
+import {AmplifyProvider} from "@aws-amplify/ui-react";
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);import { 
+  CardB 
+} from './ui-components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <NewHomes />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
