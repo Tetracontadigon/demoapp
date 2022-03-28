99ef5662a7e9ace0a01e6fbe711e09a8cbe0c94f
tetracontadi <lucan.kesyer@gmail.com>
2022-03-28 13:35:19 -0400

    headrick email march28

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Amplify from 'aws-amplify';
-import "@aws-amplify/ui-react/styles.css";
-import {AmplifyProvider} from "@aws-amplify/ui-react";
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);import { 
-  CardB 
+import { 
+  NewHomes 
 } from './ui-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
           <NewHomes />
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
